e723e1c8781176cdec563b0582d954f733e80e7b
David Rubio <davidrubiobenito@gmail.com>
2017-06-25 15:42:01 +0200

    Hasta el video 016

---
+++ src/main/webapp/angular2/src/app/app.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+    
+export class AppComponent implements OnInit
+{   
+    public submitted: boolean;
+    roomsearch: FormGroup;
+    rooms: Room[];
+    
+    
+    ngOnInit(){
+        this.roomsearch = new FormGroup({
+            checkin : new FormControl(''),
+            checkout : new FormControl('')            
+                
+        });    
+        
+        this.rooms = ROOMS;
+    }
+    
+    onSubmit({value, valid}: {value:Roomsearch, valid:boolean}){
+            console.log(value);
+    }
+        
+    reserveRoom(value:string){
+        console.log("Room id for reservation: " + value);
+    }
+    
+    
+    
+}
+
+export interface Roomsearch{
+    checkin: string;
+    checkout: string;    
+}
+
+export interface Room{
+    id: string,
+    roomNumber:string,
+    price:string,
+    links:string  
+}
+
+var ROOMS:Room[] = [
+    {
+        "id": "38932123",
+        "roomNumber": "409",
+        "price" : "20",
+        "links" : "" 
+    },
+    {
+        "id": "12345678",
+        "roomNumber": "410",
+        "price" : "25",
+        "links" : "" 
+    },{
+        "id": "87654321",
+        "roomNumber": "410",
+        "price" : "28",
+        "links" : "" 
+    },
+        
+];
+
+    
